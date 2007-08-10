67883b92e3019b97d6832d54f8577a378cd6a357
ni2yue4 <ni2yue4@899cc1ba-3132-0410-b4d3-a155b03da046>
2007-08-10 08:10:07 +0000

    change server_settings to smtp_settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
-ActionMailer::Base.server_settings = {
+ActionMailer::Base.smtp_settings = {
   :address => "localhost",
   :port => "25",
   :domain => "localhost.localdomain",
