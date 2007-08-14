9c075ac02b49cff558fa840ad1cb8613818707bb
ni2yue4 <ni2yue4@899cc1ba-3132-0410-b4d3-a155b03da046>
2007-08-14 12:44:19 +0000

    basic comments function done.

--- trunk/app/models/zann.rb
+++ trunk/app/models/zann.rb
@@ -1,3 +1,6 @@
 class Zann < ActiveRecord::Base
   belongs_to :zanner, :class_name => 'User', :foreign_key => 'zanner_id'
+  validates_presence_of :zannee_id
+  validates_inclusion_of :zannee_type, :in => %w{ photo }
+  validates_numericality_of :zannee_id, :only_integer => true
 end
