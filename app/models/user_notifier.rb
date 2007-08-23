58827ae58df0dea558feda9cbfc80a25fc35e2be
ni2yue4 <ni2yue4@899cc1ba-3132-0410-b4d3-a155b03da046>
2007-08-23 14:31:37 +0000

    email wording change

--- app/models/user_notifier.rb
+++ app/models/user_notifier.rb
@@ -1,21 +1,22 @@
 class UserNotifier < ActionMailer::Base
+  ZANN_SERVER_ADDRESS = '152.62.42.58'
   def signup_notification(user)
     setup_email(user)
-    @subject    += 'Please activate your new account'
-    @body[:url]  = "http://localhost:3002/account/activate/#{user.activation_code}"
+    @subject    += 'Registration Verification'
+    @body[:url]  = "http://#{ZANN_SERVER_ADDRESS}:3000/account/activate/#{user.activation_code}"
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://localhost:3002/"
+    @body[:url]  = "http://#{ZANN_SERVER_ADDRESS}:3000/main"
   end
   
   protected
   def setup_email(user)
     @recipients  = "#{user.email}"
     @from        = "donotreply@emccrdc.com"
-    @subject     = "[ZANN] Welcome to zann "
+    @subject     = "[ZANN]"
     @sent_on     = Time.now
     @body[:user] = user
   end
