64d5fcb1b9568e90c7572011a078b0e796a940fd
ni2yue4 <ni2yue4@899cc1ba-3132-0410-b4d3-a155b03da046>
2007-08-24 02:46:59 +0000

    complete data validation

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,7 +5,7 @@ class Photo < ActiveRecord::Base
   validates_filesize_of :image, :in => 0..50.megabytes
   belongs_to :creator, :class_name => 'User', :foreign_key => 'creator_id'
   belongs_to :album, :class_name => 'Album', :foreign_key => 'album_id'
-  validates_presence_of :name,:created_at
+  validates_presence_of :name,:album_id, :image
   validates_length_of :name, :maximum => 100
   def find_comments
     Comment.find(:all, :conditions => "comment_object_type = 'photo' AND comment_object_id = #{id}")
