fe256dc05a9f48810deb914d730dd9ac865de497
ni2yue4 <ni2yue4@899cc1ba-3132-0410-b4d3-a155b03da046>
2007-08-30 15:39:00 +0000

    complete deleting photos feature, currently, if a photo is deleted, all the related comments and zanns will be deleted too.

--- trunk/test/unit/photo_test.rb
+++ trunk/test/unit/photo_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class PhotoTest < Test::Unit::TestCase
-  fixtures :photos, :zanns
+  fixtures :photos, :zanns, :comments, :users, :roles, :roles_users
   
   def setup
     setup_fixture_files
@@ -16,6 +16,16 @@ class PhotoTest < Test::Unit::TestCase
     assert_equal 20, photo_shanghai.view_count
     photo_shanghai.view_once
     assert_equal 21, photo_shanghai.view_count
+  end
+  def test_destroy
+    commented_photo = photos(:shanghai_2)
+    assert_equal 2, Comment.find(:all).length
+    commented_photo.destroy
+    assert_equal 0, Comment.find(:all).length
+    zanned_photo = photos(:shanghai_1)
+    assert_equal 2, Zann.find(:all).length
+    zanned_photo.destroy
+    assert_equal 0, Zann.find(:all).length
   end
   def teardown
     teardown_fixture_files
