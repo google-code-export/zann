b386e42c039cd2639c6031ec9876331ba466222f
ni2yue4 <ni2yue4@899cc1ba-3132-0410-b4d3-a155b03da046>
2007-09-27 00:56:11 +0000

    pass all the tests in jruby+derby

--- test/functional/zann_controller_test.rb
+++ test/functional/zann_controller_test.rb
@@ -11,12 +11,17 @@ class ZannControllerTest < Test::Unit::TestCase
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
     @request.session[:user] = users(:samuel)
+    setup_fixture_files
   end
 
   def test_create
     amount = Zann.count
     post :new, {:zannee_type => 'photo', :zannee_id => 1}
     assert_equal amount + 1, Zann.count
-    assert_template "photos/zann_counter"
+    assert_template "photos/_zann_counter"
+  end
+  
+  def teardown
+    teardown_fixture_files
   end
 end
