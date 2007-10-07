e54df18ae63d8d5ebc506006f0d33439ee487c7b
ni2yue4 <ni2yue4@899cc1ba-3132-0410-b4d3-a155b03da046>
2007-10-07 11:29:02 +0000

    svn test

--- app/controllers/snacks_controller.rb
+++ app/controllers/snacks_controller.rb
@@ -3,4 +3,8 @@ class SnacksController < ApplicationController
     @snacks = Snack.find(:all,  :order => 'created_at DESC')
     @snack_pages, @snacks = paginate_collection @snacks, :page => params[:page]
   end
+
+  def filter
+    
+  end
 end
