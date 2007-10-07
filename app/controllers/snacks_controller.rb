feb755c685ec484c901c83ad749ca5516bf4ea7a
ni2yue4 <ni2yue4@899cc1ba-3132-0410-b4d3-a155b03da046>
2007-10-07 13:47:18 +0000

    prepare for snack live filter

--- app/controllers/snacks_controller.rb
+++ app/controllers/snacks_controller.rb
@@ -8,5 +8,24 @@ class SnacksController < ApplicationController
     @snacks = Snack.find(:all,  :order => 'created_at DESC')
     @snack_pages, @snacks = paginate_collection @snacks, :page => params[:page]
   end
+
+  def new
+    @snack = Snack.new
+  end
   
+  def create
+    @snack = Snack.new(params[:snack])
+    @snack.creator_id = current_user.id
+    if @snack.save
+      @snack.accepts_role 'creator', current_user
+      flash[:notice] = 'Snack was successfully created.'
+      redirect_to :action => 'list'
+    else
+      render :action => 'new'
+    end
+  end
+
+  def show
+    @snack = Snack.find(params[:id])
+  end
 end
