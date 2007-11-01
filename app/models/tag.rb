d507f014ea81e85608e6d20efeec92855fb40cf6
ni2yue4 <ni2yue4@899cc1ba-3132-0410-b4d3-a155b03da046>
2007-11-01 10:01:15 +0000

    begin to add tag cloud support

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,15 @@ class Tag < ActiveRecord::Base
     :from => [:photos, :snacks], 
     :through => :taggings,
     :dependent => :destroy
+
+  def self.cloud(args = {})
+    find(:all, 
+      :select => 'tags.*, count(*) as popularity',
+      :limit => args[:limit] || 5,
+      :joins => 'JOIN taggings ON taggings.tag_id = tags.id',
+      :conditions => args[:conditions],
+      :group => 'taggings.tag_id',
+      :order => 'popularity DESC'
+    )
+  end
 end
