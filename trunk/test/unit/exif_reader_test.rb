ba8a8c6df2bc32a1e1bac6308e50dc57c435abd9
ni2yue4 <ni2yue4@899cc1ba-3132-0410-b4d3-a155b03da046>
2007-11-13 09:51:08 +0000

    basic EXIF support

--- trunk/test/unit/exif_reader_test.rb
+++ trunk/test/unit/exif_reader_test.rb
@@ -6,7 +6,17 @@ class ExifReaderTest < Test::Unit::TestCase
   end
 
   def test_read_exif
-    ExifReader.read()
+    exif_info = ExifReader.read(File.join(RAILS_ROOT, 'test', 'unit', 'data', 'test.jpg'))
+    assert_equal "'NIKON CORPORATION'", exif_info[:make]
+    assert_equal "'NIKON D50'", exif_info[:model]
+    assert_equal "500", exif_info[:pixel_x_dimension]
+    assert_equal "332", exif_info[:pixel_y_dimension]
+    assert_equal "'Adobe Photoshop 7.0'", exif_info[:software]
+    assert_equal "8", exif_info[:fnumber]
+    assert_equal "10/80", exif_info[:exposure_time]
+    assert_equal "0", exif_info[:flash]
+    assert_equal "38", exif_info[:focal_length]
+    assert_equal "0", exif_info[:white_balance]
   end
 
   def teardown
