0465fe14f1522e4b10e127dedcd67eef77e30479
ni2yue4 <ni2yue4@899cc1ba-3132-0410-b4d3-a155b03da046>
2007-12-16 15:18:03 +0000

    add an empty test for video

--- test/unit/video_test.rb
+++ test/unit/video_test.rb
@@ -3,5 +3,8 @@ require File.dirname(__FILE__) + '/../test_helper'
 class VideoTest < Test::Unit::TestCase
   include VideoProcessing
   fixtures :videos
-
+  
+  def test_truth
+    assert true
+  end
 end
