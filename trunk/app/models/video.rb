146cb12603e57ac08ecc95343d34b97876fdb7a7
ni2yue4 <ni2yue4@899cc1ba-3132-0410-b4d3-a155b03da046>
2007-12-16 12:13:45 +0000

    finish most video functionality

--- trunk/app/models/video.rb
+++ trunk/app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ActiveRecord::Base
   acts_as_authorizable
-  file_column :movie
+  file_column :movie, :video => {}
   validates_filesize_of :movie, :in => 0..500.megabytes
   belongs_to :creator, :class_name => 'User', :foreign_key => 'creator_id'
   validates_presence_of :name,:movie
