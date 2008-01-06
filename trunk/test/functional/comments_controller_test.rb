04adbef8cd38fc3180c9205c1ca69e9112d41fa3
ni2yue4 <ni2yue4@899cc1ba-3132-0410-b4d3-a155b03da046>
2008-01-06 06:10:53 +0000

    add a feature to comment video

--- trunk/test/functional/comments_controller_test.rb
+++ trunk/test/functional/comments_controller_test.rb
@@ -12,7 +12,23 @@ class CommentsControllerTest < Test::Unit::TestCase
   end
 
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_post_photo_comment
+    comment_count = Comment.count
+    post :new, :comment => {
+      :content => 'comment test',
+      :comment_object_type => 'photo',
+      :comment_object_id => 1
+    }
+    assert comment_count + 1, Comment.count
+  end
+
+  def test_post_video_comment
+    comment_count = Comment.count
+    post :new, :comment => {
+      :content => 'comment test',
+      :comment_object_type => 'video',
+      :comment_object_id => 1
+    }
+    assert comment_count + 1, Comment.count
   end
 end
