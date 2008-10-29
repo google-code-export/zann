5db6552fc7077b3d9f40d3e6e4e61e5d64168118
ni2yue4 <ni2yue4@899cc1ba-3132-0410-b4d3-a155b03da046>
2008-10-29 13:36:07 +0000

    remove goldspike and ready for 0.3.1 release

--- trunk/config/war.rb
+++
@@ -1,4 +0,0 @@
-maven_library 'org.apache.derby', 'derbyclient', '10.3.1.4'
-#maven_library 'mysql', 'mysql-connector-java', '5.0.5'
-add_gem 'rails', '= 1.2.3'
-#add_gem 'activerecord-jdbc-adapter'
